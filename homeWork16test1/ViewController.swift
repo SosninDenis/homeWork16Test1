7d2efc7eea872d605fb475caffad62ee88e3e4c8
Sosnin Denis <sosnin.den@gmail.com>
2022-11-24 23:07:50 +0300

    first commit

---
+++ homeWork16test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork16test1
+//
+//  Created by Денис Соснин on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
